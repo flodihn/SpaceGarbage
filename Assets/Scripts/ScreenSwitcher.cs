d7837ddaae04c135c4ef10d1241b240202c3b21a
Christian Flodihn <christian@flodihn.se>
2015-01-24 17:44:13 +0100

    Added new screen switcher, changing between ship building, scanner and info.

---
+++ Assets/Scripts/ScreenSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSwitcher : MonoBehaviour {
+	public GameObject[] screens;
+	int screenIndex = 0;
+	GameObject currentScreen;
+
+	void Start()
+	{
+		currentScreen = screens[screenIndex];
+		currentScreen.SetActive(true);
+	}
+
+	public void NextScreen()
+	{
+		currentScreen.SetActive(false);
+		screenIndex++;
+		if(screenIndex > screens.Length - 1)
+			screenIndex = 0;
+		currentScreen = screens[screenIndex];
+		currentScreen.SetActive(true);
+	}
+
+	public void PreviousScreen()
+	{
+		currentScreen.SetActive(false);
+		screenIndex--;
+		if(screenIndex < 0)
+			screenIndex = screens.Length - 1;
+		currentScreen = screens[screenIndex];
+		currentScreen.SetActive(true);
+	}
+}
